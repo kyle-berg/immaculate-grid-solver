c1c32f30d979a9804883ff747ca027e3e326c3c1
kyle-berg <khb120@gmail.com>
2024-01-04 17:30:19 -0500

    added positions as a search criteria

--- main.py
+++ main.py
@@ -6,14 +6,52 @@ df_master = pd.read_csv("Appearances.csv")
 print("Master dataframe loaded")
 
 while True:
-    team1 = input("Enter Team 1 ID: ")
+    team1 = input("Enter Team 1 ID/POSITION: ")
     team2 = input("Enter Team 2 ID: ")
 
-    df_t1 = df_master.loc[df_master['teamID'] == team1]
-    df_t2 = df_master.loc[df_master['teamID'] == team2]
+    if team1 == "C":
+        df_both = df_master.loc[(df_master['G_c'] > 1) & (df_master['teamID'] == team2)]
+        df_both = df_both.sort_values("G_all", ascending=True)
 
-    df_both = pd.merge(df_t1, df_t2, on='playerID')
-    df_both = df_both.sort_values("G_all_x", ascending=True)
+    elif team1 == "P":
+        df_both = df_master.loc[(df_master['G_p'] > 1) & (df_master['teamID'] == team2)]
+        df_both = df_both.sort_values("G_all", ascending=True)
+
+    elif team1 == "1B":
+        df_both = df_master.loc[(df_master['G_1b'] > 1) & (df_master['teamID'] == team2)]
+        df_both = df_both.sort_values("G_all", ascending=True)
+
+    elif team1 == "2B":
+        df_both = df_master.loc[(df_master['G_2b'] > 1) & (df_master['teamID'] == team2)]
+        df_both = df_both.sort_values("G_all", ascending=True)
+
+    elif team1 == "3B":
+        df_both = df_master.loc[(df_master['G_3b'] > 1) & (df_master['teamID'] == team2)]
+        df_both = df_both.sort_values("G_all", ascending=True)
+
+    elif team1 == "SS":
+        df_both = df_master.loc[(df_master['G_ss'] > 1) & (df_master['teamID'] == team2)]
+        df_both = df_both.sort_values("G_all", ascending=True)
+
+    elif team1 == "LF":
+        df_both = df_master.loc[(df_master['G_lf'] > 1) & (df_master['teamID'] == team2)]
+        df_both = df_both.sort_values("G_all", ascending=True)
+
+    elif team1 == "CF":
+        df_both = df_master.loc[(df_master['G_cf'] > 1) & (df_master['teamID'] == team2)]
+        df_both = df_both.sort_values("G_all", ascending=True)
+
+    elif team1 == "RF":
+        df_both = df_master.loc[(df_master['G_rf'] > 1) & (df_master['teamID'] == team2)]
+        df_both = df_both.sort_values("G_all", ascending=True)
+
+    else:
+        df_t1 = df_master.loc[df_master['teamID'] == team1]
+        df_t2 = df_master.loc[df_master['teamID'] == team2]
+        
+        df_both = pd.merge(df_t1, df_t2, on='playerID')
+        df_both = df_both.sort_values("G_all_x", ascending=True)
+        
     print(df_both.to_string())
 
     player = df_both["playerID"].iloc[0]
